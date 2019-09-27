c4c5ae4043b19dfc9a5d481e20c95b864230484e
ahmedtamer65 <40834972+ahmedtamer65@users.noreply.github.com>
2019-09-27 06:34:32 -0400

    Add files via upload

---
+++ port_Scanner_SCAR.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+###############
+import socket
+import os
+import random
+import sys
+from time import *
+from datetime import datetime
+####################
+####################
+#...USE==> python3 to run...!#
+#codded by SCAR (AT)
+####################
+os.system ("clear")
+ip=input (" ENTER YOUR IP TO START=====>: ")
+t1=datetime.now()
+print (" Scanning Start... %s Please Wait..! "%ip)
+sleep(1)
+####################
+try:
+    for port in range(1,6553):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        if(s.connect_ex((ip,port))==0):
+            try:
+                serv=socket.getservbyport(port)
+
+            except socket.error:
+                serv="Unknown Service"
+            print ("Port %s Open Service:%s "%(port,serv))
+        t2=datetime.now()
+        t3=t2-t1
+    print (" Scanning Completed On %s"%t3)
+    print (" see you soon....!.")
+    print (" Codded by SCAR (AT)")
+except KeyboardInterrupt:
+    print(" See You Soon....!")
+    print(" Thank you....!")
+    print(" codded by SCAR (AT)")
+###############################
+
